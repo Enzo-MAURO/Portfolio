075de02e3558a679c01a364ca9802c2e385cd448
Enzo-MAURO <e.mauro@ecole-ipssi.net>
2026-05-21 10:06:20 +0200

    commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Animation apparition sections
+
+const sections = document.querySelectorAll(".section");
+
+window.addEventListener("scroll", () => {
+
+    sections.forEach(section => {
+
+        const sectionTop = section.getBoundingClientRect().top;
+
+        if (sectionTop < window.innerHeight - 100) {
+
+            section.classList.add("show");
+
+        }
+
+    });
+
+});
